a7dd74507a951458512991f8e5489a0243b123bd
IASP23 <alexanderpilataxi232002@gmail.com>
2024-03-31 22:59:44 -0500

    Loading of components templates

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        {/*  <Sidebar /> */} {/* Por Corregir  */}
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
